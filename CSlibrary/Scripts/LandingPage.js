23c57050ec4c24a1a63b805b122a145c12ee918c
justinervin <garcia911_justin@yahoo.com>
2014-08-26 20:38:44 +0800

    all files

---
+++ CSlibrary/Scripts/LandingPage.js
@@ -0,0 +1,38 @@
+﻿$(document).ready(function () {
+    $(window).scroll(function () {
+        $('#img1').each(function () {
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).addClass("fadeIn");
+            }
+        });
+
+        $('#img2').each(function () {
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).addClass("slideRight");
+            }
+        });
+
+        $('#img3').each(function () {
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
+                $(this).addClass("pullUp");
+            }
+        });
+    });
+
+    $('#login-form').on('shown.bs.modal', function () {
+        $('#loginId').focus();
+    });
+
+    $('#signup-form').on('shown.bs.modal', function () {
+        $('#regId').focus();
+    });
+});
